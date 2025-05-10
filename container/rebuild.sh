bb3758577b151f2bd81c12d9cc77eb236e29e0c0
Norest <sqidde@gmail.com>
2025-05-10 12:37:28 +0300

    ros container scripts upgrade

--- container/rebuild.sh
+++ container/rebuild.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Stop and remove old container (if exists)
+# Stop and remove old container
 docker stop ros_noetic_gpu 2>/dev/null
 docker rm ros_noetic_gpu 2>/dev/null
 
-# Rebuild the Docker image
-docker build -t ros_noetic_nvidia:latest .
+# Rebuild with host user args
+docker build \
+  -f ./container/Dockerfile \
+  --build-arg USERNAME=$(whoami) \
+  --build-arg USER_UID=$(id -u) \
+  --build-arg USER_GID=$(id -g) \
+  -t ros_noetic_nvidia:latest .
 
-echo "Rebuild complete. Run ./start_container.sh to start."
+echo "Rebuild complete. Run container/run.sh to start."
--- container/run.sh
+++ container/run.sh
@@ -1,15 +1,51 @@
 #!/bin/bash
 
-# Allow X11 access (run on host machine first)
+# Allow X11 access
 xhost +local:docker
 
-# Start the container with GPU and X11 forwarding
-docker run -it \
-  --gpus all \
-  --env="DISPLAY" \
-  --env="QT_X11_NO_MITSHM=1" \
-  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-  --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
-  --net=host \
-  --name ros_noetic_gpu \
-  ros_noetic_nvidia:latest
+# Get current user/group info
+USER_ID=$(id -u)
+GROUP_ID=$(id -g)
+USERNAME=$(whoami)
+WORKDIR=$(pwd)
+CONTAINER_NAME="ros_noetic_gpu"
+IMAGE_NAME="ros_noetic_nvidia:latest"
+
+# Check if container exists
+if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
+    # Container exists
+    if [ "$(docker ps -q -f name=$CONTAINER_NAME)" ]; then
+        # Container is running, attach to it
+        echo "Container $CONTAINER_NAME is already running. Attaching..."
+        docker exec -it \
+            --user="$USER_ID:$GROUP_ID" \
+            -e DISPLAY \
+            -e QT_X11_NO_MITSHM=1 \
+            $CONTAINER_NAME bash
+    else
+        # Container exists but stopped, start and attach
+        echo "Container $CONTAINER_NAME exists but is stopped. Starting..."
+        docker start $CONTAINER_NAME
+        docker exec -it \
+            --user="$USER_ID:$GROUP_ID" \
+            -e DISPLAY \
+            -e QT_X11_NO_MITSHM=1 \
+            $CONTAINER_NAME bash
+    fi
+else
+    # Container doesn't exist, create and run
+    echo "Creating new container $CONTAINER_NAME..."
+    docker run -it \
+      --gpus all \
+      --env="DISPLAY" \
+      --env="QT_X11_NO_MITSHM=1" \
+      --env="USER_ID=$USER_ID" \
+      --env="GROUP_ID=$GROUP_ID" \
+      --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+      --volume="$HOME/.Xauthority:/home/$USERNAME/.Xauthority:rw" \
+      --volume="$WORKDIR:$WORKDIR" \
+      --workdir="$WORKDIR" \
+      --user="$USER_ID:$GROUP_ID" \
+      --name $CONTAINER_NAME \
+      $IMAGE_NAME
+fi
