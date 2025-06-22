8f6380c5ce165f9502e00b4248e45f3d28ce133e
Norest <sqidde@gmail.com>
2025-06-22 17:25:18 +0300

    robot stuff

--- robot/ros_ws/src/ainex_bringup/scripts/source_env.bash
+++ robot/ros_ws/src/ainex_bringup/scripts/source_env.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 sleep 10
 export ROS_IP=192.168.149.1
-export ROS_MASTER_URI=http://ubuntu:11311
+export ROS_MASTER_URI=http://192.168.149.1:11311
 export ROS_HOSTNAME=$ROS_IP
 
 export working_space=/home/ubuntu
