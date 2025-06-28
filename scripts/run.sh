f500e5fcaa4034886a3d1abcb65a1fbf40d6c19c
Norest <sqidde@gmail.com>
2025-06-28 18:28:15 +0300

    ros ip fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,7 @@ WORKDIR=$(pwd)
 CONTAINER_NAME="ros_noetic_gpu"
 IMAGE_NAME="ros_noetic_nvidia:latest"
 ROBOT_IP=192.168.149.1
+PC_IP=192.168.149.100
 
 # Check if container exists
 if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
@@ -34,7 +35,7 @@ else
         --env="USER_ID=$USER_ID" \
         --env="GROUP_ID=$GROUP_ID" \
         --env="ROS_MASTER_URI=http://$ROBOT_IP:11311" \
-        --env="ROS_IP=$ROBOT_IP" \
+        --env="ROS_IP=$PC_IP" \
         --env="PYTHONPATH=$WORKDIR/pc:$PYTHONPATH" \
         --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
         --volume="$HOME/.Xauthority:/home/$USERNAME/.Xauthority:rw" \
